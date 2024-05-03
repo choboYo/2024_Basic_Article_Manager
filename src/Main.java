6d4a3aa121db67afabc4b75cfb8f07e25923b145
choboYo <tjddlsmn96@gmail.com>
2024-05-03 16:26:29 +0900

    article write 구현

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		List<Article> articles = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
 		int indexNumber = 0;
 		System.out.println("== 프로그램 시작 ==");
@@ -17,17 +20,20 @@ public class Main {
 				break;
 			} else if (cmd.equals("article list")) {
 
-				System.out.println("게시글이 없습니다.");
+				System.out.println("게시물이 없습니다.");
 			} else if (cmd.equals("article write")) {
 
 				System.out.printf("제목 : ");
 				String title = sc.nextLine().trim();
-				
+
 				System.out.printf("내용 : ");
 				String contents = sc.nextLine().trim();
 
-				indexNumber++;
 
+				Article article = new Article(title, contents, indexNumber);
+				articles.add(article);
+				
+				indexNumber++;
 				System.out.printf("%d번 글이 생성 되었습니다.", indexNumber);
 
 			} else if (cmd.length() == 0) {
@@ -45,4 +51,20 @@ public class Main {
 		System.out.println("== 프로그램 종료==");
 	}
 
+}
+class Article {
+	
+	String title;
+	String contents;
+	int	indexNumber;
+	
+	Article (String title, String contents, int indexNumber) {
+		
+		this.title = title;
+		this.contents = contents;
+		this.indexNumber = indexNumber;
+		
+	}
+	
+	
 }
