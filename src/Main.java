c6f4dd05a364c51f1d65679c1bff9cca939cd0a5
choboYo <tjddlsmn96@gmail.com>
2024-05-04 23:40:54 +0900

    복습 및 태스트용 데이터 만들기

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		List<Article> articles = new ArrayList<>();
+		// List ↑ class 타입 매개변수 = Article 타입의 객체를 저장하는 ArrayList<>();
+		// 객체가 인덱스로 관리되며 배열과 다른점은 저장 용량이 초과하게 되면 
+		//자동으로 양이 늘어나게 된다.
 		Scanner sc = new Scanner(System.in);
 		int indexNumber = 1;
 		System.out.println("== 프로그램 시작 ==");
@@ -18,7 +21,24 @@ public class Main {
 
 			if (cmd.equals("종료")) {
 				break;
-			} else if (cmd.equals("article list")) {
+			} else if (cmd.equals("test")) {
+				
+				for(int i = 1; i < 4; i++) {
+					
+					String title = "태스트" + i;
+					String contents = "태스트" + i;
+					
+					
+					Article article = new Article(title, contents, indexNumber);
+					articles.add(article);
+					indexNumber++;
+				}
+				
+			}
+			
+			
+			
+			else if (cmd.equals("article list")) {
 
 					if (articles.size() == 0) {
 						System.out.println("존재한는 게시글이 없습니다.");
@@ -26,7 +46,7 @@ public class Main {
 				}
 						System.out.println("번호 | 제목 | 내용");
 						for (int i = articles.size() - 1; i >= 0; i--) {
-							Article article = articles.get(i);
+							Article article = articles.get(i); // List를 통해서 저장되어있는 내용을 Article 객체를 통해서 조립하는 상황
 							System.out.printf("%d    | %s    | %s\n", article.indexNumber, article.title, article.contents);
 					}
 					
