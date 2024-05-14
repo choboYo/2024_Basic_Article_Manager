440cac232364dc609088508de858f9b012ddfae0
choboYo <tjddlsmn96@gmail.com>
2024-05-14 21:49:46 +0900

    doLogout, isLogined

--- src/com/koreaIT/BAM/controller/Controller.java
+++ src/com/koreaIT/BAM/controller/Controller.java
@@ -2,11 +2,14 @@ package com.koreaIT.BAM.controller;
 
 import java.util.Scanner;
 
+import com.koreaIT.BAM.dto.Member;
+
 public abstract class Controller {
 	
 	public Scanner sc;
 	public int lastId;
 	public String cmd;
+	public Member loginedMember;
 	
 	public abstract void doAction(String cmd, String methodName);
 	public abstract void makeTestData();
